50e659d4b0e0b9006e3853de12a2bce9ff076743
Sebastian Ljungberg <sebastian.ljungberg@weselect.com>
2024-03-20 15:21:12 +0100

    Add Docker Compose file and Prisma configuration

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
-@Controller({})
-export class AuthController {}
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('signup')
+  signUp() {
+    return this.authService.signUp();
+  }
+
+  @Post('login')
+  signIn() {
+    return this.authService.login();
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable({})
-export class AuthService {}
+export class AuthService {
+  login() {
+    return 'I am in login method of auth controller';
+  }
+
+  signUp() {
+    return { msg: 'I am in signup method of auth controller', test: 'test' };
+  }
+}
