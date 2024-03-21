befec60621c224de0baa43ab16240d35ec3f17c1
Sebastian Ljungberg <sebastian.ljungberg@weselect.com>
2024-03-21 15:34:36 +0100

    Refactor signUp method in AuthService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
 import { User, Bookmark } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class AuthService {
@@ -12,17 +16,31 @@ export class AuthService {
   }
 
   async signUp(dto: AuthDto) {
-    // generate hash of password
-    const hash = await argon.hash(dto.password);
-    // create user
-    const user = await this.prisma.user.create({
-      data: {
-        email: dto.email,
-        hash,
-      },
-    });
-    delete user.hash;
+    try {
+      // generate hash of password
+      const hash = await argon.hash(dto.password);
+      // create user
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+      delete user.hash;
 
-    return user;
+      return user;
+    } catch (error) {
+      if (
+        error instanceof
+        PrismaClientKnownRequestError
+      ) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException(
+            'User already exists',
+          );
+        }
+      }
+    }
+    throw Error;
   }
 }
