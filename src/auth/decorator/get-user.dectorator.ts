3a863226a1adcea518c2d40d4557d759737f487e
Sebastian Ljungberg <sebastian.ljungberg@weselect.com>
2024-03-22 16:11:38 +0100

    Add GetUser decorator and use it in UserController

---
+++ src/auth/decorator/get-user.dectorator.ts
@@ -0,0 +1,17 @@
+import {
+  ExecutionContext,
+  createParamDecorator,
+} from '@nestjs/common';
+
+export const GetUser = createParamDecorator(
+  (
+    data: string | undefined,
+    context: ExecutionContext,
+  ) => {
+    const request: Express.Request = context
+      .switchToHttp()
+      .getRequest();
+    if (data) return request.user[data];
+    return request.user;
+  },
+);
