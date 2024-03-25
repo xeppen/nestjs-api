73e71446da9ec5d2344fec7d84de384188c0c23e
Sebastian Ljungberg <sebastian.ljungberg@weselect.com>
2024-03-25 16:03:50 +0100

    Add pactum library for API testing and implement signup and login endpoints

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,11 +1,12 @@
 import { Test } from '@nestjs/testing';
+import * as pactum from 'pactum';
 import { AppModule } from '../src/app.module';
 import {
   INestApplication,
   ValidationPipe,
 } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
 import { PrismaService } from '../src/prisma/prisma.service';
+import { AuthDto } from 'src/auth/dto';
 
 describe('App e2e', () => {
   let app: INestApplication;
@@ -24,8 +25,12 @@ describe('App e2e', () => {
       }),
     );
     await app.init();
+    await app.listen(3333);
     prisma = app.get(PrismaService);
     await prisma.cleanDb();
+    pactum.request.setBaseUrl(
+      'http://localhost:3333',
+    );
   });
 
   afterAll(async () => {
@@ -33,30 +38,46 @@ describe('App e2e', () => {
   });
 
   describe('Auth', () => {
+    const dto: AuthDto = {
+      email: 'xeppen@gmail.com',
+      password: '123456',
+    };
     describe('Signup', () => {
-      it.todo('Should create user');
+      it('Should create user', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody(dto)
+          .expectStatus(201);
+      });
+
+      describe('Login', () => {
+        it('Should login', () => {
+          return pactum
+            .spec()
+            .post('/auth/login')
+            .withBody(dto)
+            .expectStatus(200);
+        });
+      });
     });
 
-    describe('Login', () => {
-      it.todo('Should login');
+    describe('User', () => {
+      describe('Get me', () => {});
+
+      describe('Edit user', () => {});
     });
-  });
 
-  describe('User', () => {
-    describe('Get me', () => {});
+    describe('Bookmarks', () => {
+      describe('Create bookmark', () => {});
 
-    describe('Edit user', () => {});
-  });
+      describe('Get bookmark', () => {});
 
-  describe('Bookmarks', () => {
-    describe('Create bookmark', () => {});
+      describe('Get bookmark by id', () => {});
 
-    describe('Get bookmark', () => {});
+      describe('Edit bookmark', () => {});
 
-    describe('Get bookmark by id', () => {});
-
-    describe('Edit bookmark', () => {});
-
-    describe('Delete bookmark', () => {});
+      describe('Delete bookmark', () => {});
+    });
   });
 });
